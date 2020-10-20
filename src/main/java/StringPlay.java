808844fe68f6cf139101d17c67c5da450d0ac34f
Caroline E <caroline.esquivel01@gmail.com>
2020-10-20 13:53:13 -0500

    first commit

--- src/main/java/StringPlay.java
+++ src/main/java/StringPlay.java
@@ -12,7 +12,21 @@ public class StringPlay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a letter: ");
-        String letter = scanner.next();
+        String letter = scanner.nextLine();
         System.out.println(capitalizeLetter(letter));
+
+        System.out.println("Please enter a string: ");
+        String input = scanner.nextLine();
+        System.out.println("You entered: \""+input+"\"");
+
+        if (StringUtils.isNumeric(input)) {
+            System.out.println("\""+input+"\" is a number");
+        }else{
+            System.out.println("\""+input+"\" is not a number");
+        }
+
+        System.out.println("Flipped case: " + StringUtils.swapCase(input));
+
+        System.out.println("Reversed: " + StringUtils.reverse(input));
     }
 }
